9c6149a568b7594a4af9521fda11e830f4405fa9
heru prasetia <heru.praseti@gmail.com>
2023-12-08 10:07:48 +0700

    perubahan thema

--- app/src/main/java/com/example/bissmillah/CustomAdapter.kt
+++ app/src/main/java/com/example/bissmillah/CustomAdapter.kt
@@ -7,13 +7,9 @@ import android.widget.TextView
 import com.example.bissmillah.R
 
 class CustomAdapter(private val context: Context, private val data: List<Map<String, Any>>) : BaseAdapter() {
-
     override fun getCount(): Int = data.size
-
     override fun getItem(position: Int): Any = data[position]
-
     override fun getItemId(position: Int): Long = position.toLong()
-
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val view: View = convertView ?: inflater.inflate(R.layout.grid_data_loop, parent, false)
